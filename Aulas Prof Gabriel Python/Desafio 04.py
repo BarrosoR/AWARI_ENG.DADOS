e23b944877aead662e1b2af7988b190b4103b9e0
Raul Barroso <89712072+BarrosoR@users.noreply.github.com>
2024-01-08 10:05:25 -0300

    Continuação Desafio 04

---
+++ Aulas Prof Gabriel Python/Desafio 04.py
@@ -0,0 +1,32 @@
+#Exercícios lógicos em grupo
+
+#1 - Faça um programa que leia duas notas de um aluno e imprima na tela sua média.
+
+#Inserindo as notas
+nota1= float(input("Digite a primeira nota: "))
+nota2= float(input("Digite a segunda nota: "))
+
+#Calculo da media
+
+media= (nota1+nota2)/2
+
+print("A media desse estudante foi", media)
+
+
+
+#2 - Faça um programa que receba um valor em metros e o exiba convertido para centímetros e milímetros.
+
+metros = float(input("Digite o valor em métros: "))
+
+milímetros = metros * 1000
+print("%10.2f metros equivalem a %10.3f milímetros." % (metros,milímetros))
+
+
+
+#3 - Faça um programa que receba um valor em real e converta para dólar
+
+
+
+
+
+#4 - Faça um programa que cálcule a área de um círculo.
